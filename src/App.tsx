b94b11923bca862d0c85776b133ba4732dc465e3
Sergei <Sergei.stolpovskikh@gmail.com>
2024-11-24 15:15:55 +0300

    storage practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react';
 import './App.css';
 import { BearCounter } from './BearCounter';
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
 			<BearCounter />
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface IBearStore {
 	bears: number;
@@ -6,8 +7,21 @@ interface IBearStore {
 	removeAllBears: () => void;
 }
 
-export const useBearStore = create<IBearStore>(set => ({
-	bears: 0,
-	increasePopulation: () => set(state => ({ bears: state.bears + 1 })),
-	removeAllBears: () => set({ bears: 0 }),
-}));
+// export const useBearStore = create<IBearStore>(set => ({
+// 	bears: 0,
+// 	increasePopulation: () => set(state => ({ bears: state.bears + 1 })),
+// 	removeAllBears: () => set({ bears: 0 }),
+// }));
+
+export const useBearStore = create<IBearStore>()(
+	persist(
+		set => ({
+			bears: 0,
+			increasePopulation: () => set(state => ({ bears: state.bears + 1 })),
+			removeAllBears: () => set({ bears: 0 }),
+		}),
+		{
+			name: 'bear-storage',
+		}
+	)
+);
